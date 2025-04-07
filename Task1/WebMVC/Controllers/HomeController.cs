e79e732fb98085cd3925fbc64ad29090811921ad
ilya54 <snaky54@yandex.ru>
2025-04-07 11:12:58 +0300

    Adding Task1

---
+++ Task1/WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            StudentModel student = new()
+            {
+                Surname = "Ланкин",
+                Name = "Илья",
+                Specialization = "Инженерия программного обеспечения",
+                Year = 3
+            };
+
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task1/WebMVC/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMVC.Models
+{
+    public class StudentModel
+    {
+        public string? Surname { get; set; }
+        public string? Name { get; set; }
+        public string? Specialization { get; set; }
+        public int Year { get; set; }
+    }
+}
